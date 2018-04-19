de248b285119b99fbaa097e2c18fc09fcfcd790c
Jack Huang <719668276@qq.com>
2018-04-19 20:32:47 +0800

    add mnist_app_plus.py

--- 05_fully_Connected_nn/mnist_app_plus.py
+++ 05_fully_Connected_nn/mnist_app_plus.py
@@ -8,10 +8,18 @@ import sys
 import cv2
 import os
 
+PaintSize = 350
+BgColor = (255, 255, 255)
+PaintColor = (0, 0, 0)
+StrokeWeight = 20
+
+
 drawing = False
 start = (-1, -1)
 lastPoint = (-1,-1)
-img = np.zeros((150,150,3), np.uint8)
+
+# Initialise background color to white.
+img = np.full((PaintSize, PaintSize,3), BgColor, dtype=np.uint8)
 
 # mouse callback
 def mouse_event(event, x, y, flags, param):
@@ -23,17 +31,37 @@ def mouse_event(event, x, y, flags, param):
 		start = (x, y)
 	elif event == cv2.EVENT_MOUSEMOVE:
 		if drawing:
-			# -1: fill
-			# cv2.circle(img, (x,y),5,200,-1)
-			cv2.line(img,lastPoint,(x, y), (255,255,255), 3)
-			# cv2.line(img,lastPoint,(x, y), 0, 3)
+			cv2.line(img,lastPoint,(x, y), PaintColor, StrokeWeight)
 	elif event == cv2.EVENT_LBUTTONUP:
 		drawing = False
 	lastPoint = (x, y)
 
 
 
+# Image Processing Step
+def pre_pic(picName):
+	img = Image.open(picName)
+	reIm = img.resize((28,28), Image.ANTIALIAS) # Reshape the image to the size of 28x28
 
+	im_arr = np.array(reIm.convert('L')) # convert to grayscale 
+	cv2.imshow("Little",im_arr)
+	threshold = 50
+
+	for i in range(28):
+		for j in range(28):
+			im_arr[i][j] = 255 - im_arr[i][j]
+			if (im_arr[i][j] < threshold):
+				im_arr[i][j] = 0
+			else:
+				im_arr[i][j] = 255
+
+	nm_arr = im_arr.reshape([1,784])
+	nm_arr = nm_arr.astype(np.float32)
+	img_ready = np.multiply(nm_arr, 1.0/255.0)
+	return img_ready
+
+
+# Use trained model
 # how the process worked ?
 def restore_model(testPicArr):
 	with tf.Graph().as_default() as tg:
@@ -55,56 +83,32 @@ def restore_model(testPicArr):
 				print("No checkpoint file found")
 				return -1
 
-def pre_pic(picName):
-	img = Image.open(picName)
-	print(img)
-	reIm = img.resize((28,28), Image.ANTIALIAS) # Reshape the image to the size of 28x28
-	print(reIm)
-	im_arr = np.array(reIm.convert('L')) # convert to grayscale 
-	print(im_arr)
-	threshold = 50
-
-	for i in range(28):
-		for j in range(28):
-			im_arr[i][j] = 255 - im_arr[i][j]
-			if (im_arr[i][j] < threshold):
-				im_arr[i][j] = 0
-			else:
-				im_arr[i][j] = 255
-
-	nm_arr = im_arr.reshape([1,784])
-	nm_arr = nm_arr.astype(np.float32)
-	img_ready = np.multiply(nm_arr, 1.0/255.0)
-	return img_ready
-
 
 def application():
 	global img 
-	cv2.namedWindow('Draw a number from 0 to 9:')
+	cv2.namedWindow('Press \'s\' to Save,\'c\' to clear')
 	# assign callback
-	cv2.setMouseCallback('Draw a number from 0 to 9:', mouse_event)
+	cv2.setMouseCallback('Press \'s\' to Save,\'c\' to clear', mouse_event)
 
-	print("Press Ctrl+C to quit the program:")
+	print("Press q or Esc to quit the program:")
 	while True:
-		cv2.imshow('Draw a number from 0 to 9:', img)
+		cv2.imshow('Press \'s\' to Save,\'c\' to clear', img)
 		key = cv2.waitKey(20)
+
 		if key == 27 or key == 113: # break when press ESC/q
 			break
 		elif key == 115: # s for 'save'
-			# dt = str(datetime.datetime.now())
-			# imgName = 'img_'+dt+'.png'
-			imgName = 'hand.png'
+			imgName = './pic/handWrite.png'
 			cv2.imwrite(imgName, img)
 			print(imgName + " saved")
 			testPicArr = pre_pic(imgName)
 			preValue = restore_model(testPicArr)
 			print ("The prediction number is:", preValue)
 		elif key == 99: # c for 'clear'
-			# img = np.zeros((150,150,1), np.uint8)
-			img = np.zeros((150,150,3), np.uint8)
-			# img = np.full((150, 150), 255, dtype=np.uint8)
+			img = np.full((PaintSize, PaintSize,3), BgColor, dtype=np.uint8)
 		else:
 			pass
+
 
 def main():
 	application()
